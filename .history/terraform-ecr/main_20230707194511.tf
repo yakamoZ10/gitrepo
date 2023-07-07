1a322693c14f64088fede474eecfbda015d770b2
Ardin Drejta <adrejta@3cis.net>
2023-07-07 20:24:06 +0200

    sdi

---
+++ .history/terraform-ecr/main_20230707194511.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+    region = "eu-central-1"
+    access_key = var.access_key
+    secret_key = var.secret.key
+}
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "ardin-trn"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
---
+++ .history/terraform-ecr/main_20230707195216.tf
@@ -0,0 +1,24 @@
+variable "access_key"{
+
+    description = "acces key id for the provider"
+}
+
+variable "secret_key" {
+    
+    description = "Secret key for the provider"
+}
+
+provider "aws" {
+    region = "eu-central-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "ardin-trn"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
